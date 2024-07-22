41a9c7b9b1632264a82e42d2d2ea6e7d2567dfe9
svaba1012 <stefanivanovic1012@gmail.com>
2024-07-22 11:30:22 +0200

    Setting deploy port

--- backend/app.js
+++ backend/app.js
@@ -54,8 +54,8 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.DB_CONNECTION)
   .then(() =>
-    app.listen(5000, "localhost", () => {
-      console.log("Server started on port 5000");
+    app.listen(process.env.PORT || 5000, "localhost", () => {
+      console.log(`Server started on port ${process.env.PORT || 5000}`);
     })
   )
   .catch((err) => console.log(err));
